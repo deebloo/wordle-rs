ac31eec0491ac85bf229a8dd3a1dfbab7356e902
dblue <dblue@deloitte.com>
2022-02-07 11:45:59 -0500

    move words to files

--- src/dict.rs
+++ src/dict.rs
@@ -1,31 +1,38 @@
 use rand::seq::SliceRandom;
 use std::fs::read_to_string;
 
-use crate::words::WORDS;
-
 pub struct Dict {
     pub known_words: Vec<String>,
+    pub word_pool: Vec<String>,
 }
 
 impl Dict {
     pub fn new() -> Self {
-        let dict_file = read_to_string("/usr/share/dict/words").unwrap_or(String::new());
-        let known_words: Vec<String> = dict_file.split("\n").map(|f| f.to_string()).collect();
+        let known_file = read_to_string("words_5ltr.txt").expect("could not load words file");
+        let pool_file = read_to_string("words_pool.txt").expect("could not load pool file");
 
-        Self { known_words }
+        Self {
+            known_words: known_file
+                .split("\n")
+                .map(|f| f.to_string())
+                .collect::<Vec<String>>(),
+
+            word_pool: pool_file
+                .split("\n")
+                .map(|f| f.to_string())
+                .collect::<Vec<String>>(),
+        }
     }
 
     pub fn select_word(&self) -> &str {
-        WORDS
+        self.word_pool
             .choose(&mut rand::thread_rng())
             .expect("could not select word from word list")
     }
 
     pub fn is_valid(&self, word: String) -> bool {
-        let str = word.as_str();
-
         if word.len() == 5 {
-            if WORDS.contains(&str) {
+            if self.word_pool.contains(&word) {
                 true
             } else if self.known_words.len() > 0 {
                 self.known_words.contains(&word)
--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,73 @@
 mod dict;
 mod game;
-mod words;
 
 use dict::Dict;
 use game::{Game, GameResult};
-use std::fs::{read_to_string, write};
 use std::io::{self, Write};
 
 fn main() {
-    let word_file = read_to_string("words.txt").unwrap();
-    let words: String = word_file
-        .split("\n")
-        .filter_map(|f| {
-            let val = f.to_string();
+    let dict = Dict::new();
+    let word = dict.select_word();
 
-            if val.len() == 5 {
-                Some(val + "\n")
+    let mut game = Game::new(word);
+    let mut res = GameResult::Incomplete;
+
+    println!("{}", game);
+
+    while res == GameResult::Incomplete {
+        print!("Guess: ");
+
+        io::stdout().flush().expect("could not write to stdout");
+
+        let mut valid_guess = false;
+
+        while !valid_guess {
+            let mut guess = String::new();
+
+            std::io::stdin()
+                .read_line(&mut guess)
+                .expect("Could not read value");
+
+            valid_guess = dict.is_valid(guess.trim().to_string());
+
+            if valid_guess {
+                res = game.guess(guess.as_str());
+
+                println!("{}", game);
             } else {
-                None
+                print!("Invalid word try again: ");
+
+                io::stdout().flush().expect("could not write to stdout");
             }
-        })
-        .collect();
+        }
+    }
 
-    write("./5-letter-words.txt", words).unwrap();
-
-    // let dict = Dict::new();
-    // let word = dict.select_word();
-
-    // let mut game = Game::new(word);
-    // let mut res = GameResult::Incomplete;
-
-    // println!("{}", game);
-
-    // while res == GameResult::Incomplete {
-    //     print!("Guess: ");
-
-    //     io::stdout().flush().expect("could not write to stdout");
-
-    //     let mut valid_guess = false;
-
-    //     while !valid_guess {
-    //         let mut guess = String::new();
-
-    //         std::io::stdin()
-    //             .read_line(&mut guess)
-    //             .expect("Could not read value");
-
-    //         valid_guess = dict.is_valid(guess.trim().to_string());
-
-    //         if valid_guess {
-    //             res = game.guess(guess.as_str());
-
-    //             println!("{}", game);
-    //         } else {
-    //             print!("Invalid word try again: ");
-
-    //             io::stdout().flush().expect("could not write to stdout");
-    //         }
-    //     }
-    // }
-
-    // if res == GameResult::Loose {
-    //     println!("To bad! The word was: {:?}", &word);
-    // } else {
-    //     println!("Well done!");
-    // }
+    if res == GameResult::Loose {
+        println!("To bad! The word was: {:?}", &word);
+    } else {
+        println!("Well done!");
+    }
 }
+
+// Generate word list
+// fn main() {
+//     use std::fs::{read_to_string, write};
+
+//     let word_file = read_to_string("words_all.txt").unwrap();
+//     let words: String = word_file
+//         .split("\n")
+//         .filter_map(|f| {
+//             let val = f.to_lowercase().to_string();
+
+//             if val.len() == 5 {
+//                 Some(val + "\n")
+//             } else {
+//                 None
+//             }
+//         })
+//         .collect::<String>()
+//         .trim()
+//         .to_string();
+
+//     write("words_5ltr.txt", words).unwrap();
+// }
