bc552dacb863862e64950a0da60ec15aaeb49341
dblue <dblue@deloitte.com>
2022-02-10 13:05:55 -0500

    refactor user guess

--- src/main.rs
+++ src/main.rs
@@ -3,45 +3,48 @@ mod game;
 
 use dict::Dict;
 use game::{Game, GameResult};
-use std::io::{self, Stdout, Write};
+use std::io::{self, Stdin, Stdout, Write};
 
 fn main() {
     let dict = Dict::new();
-    let word = dict.select_word();
     let stdin = io::stdin();
     let mut stdout = io::stdout();
-
-    let mut game = Game::new(word);
-    let mut res = GameResult::Incomplete;
+    let mut game = Game::new(dict.select_word());
 
     println!("{}", game);
+
+    let mut res = GameResult::Incomplete;
 
     while res == GameResult::Incomplete {
         prompt(&mut stdout, "Guess: ");
 
-        let mut valid_guess = false;
+        let guess = get_user_guess(&stdin, &mut stdout, &dict);
 
-        while !valid_guess {
-            let mut guess = String::new();
+        res = game.guess(guess.as_str());
 
-            stdin.read_line(&mut guess).expect("Could not read value");
-
-            valid_guess = dict.is_valid(guess.trim().to_string());
-
-            if valid_guess {
-                res = game.guess(guess.as_str());
-
-                println!("{}", game);
-            } else {
-                prompt(&mut stdout, "Invalid word try again: ")
-            }
-        }
+        println!("{}", game);
     }
 
     if res == GameResult::Loose {
-        println!("To bad! The word was: {:?}", &word);
+        println!("To bad! The word was: {:?}", &game.word);
     } else {
         println!("Well done!");
+    }
+}
+
+fn get_user_guess(stdin: &Stdin, stdout: &mut Stdout, dict: &Dict) -> String {
+    let mut guess = String::new();
+
+    stdin.read_line(&mut guess).expect("Could not read value");
+
+    let valid_guess = dict.is_valid(guess.trim().to_string());
+
+    if valid_guess {
+        guess
+    } else {
+        prompt(stdout, "Invalid word try again: ");
+
+        get_user_guess(stdin, stdout, dict)
     }
 }
 
